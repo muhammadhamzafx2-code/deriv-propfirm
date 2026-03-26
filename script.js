00ea8616c84dc37d4d29bcd86adb98f537f7c2b0
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-03-26 18:23:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -77,6 +77,6 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     
     // Redirect after 2.5s
     setTimeout(() => {
-        window.location.href = 'https://app.deriv.com/';
+        window.location.href = 'server.html';
     }, 2500);
 });
