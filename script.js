2044bef65eb46969685c151a5c7b78a6d4a0dc72
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-03-28 08:34:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -77,6 +77,6 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     
     // Redirect after 2.5s
     setTimeout(() => {
-        window.location.href = 'xmvpropfirm.onrender.com';
+        window.location.href = 'https://xmvpropfirm.onrender.com';
     }, 2500);
 });
